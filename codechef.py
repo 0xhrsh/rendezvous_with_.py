62fef5904396622a664f9a70f6bb9ef75f2ddf15
Harsh Anand <44001232+harsh-not-haarsh@users.noreply.github.com>
2019-05-27 17:38:16 +0530

    Add files via upload

--- codechef.py
+++ codechef.py
@@ -1,13 +1,26 @@
-from bs4 import BeautifulSoup     #try replaceing bs4 with BeautifulSoup (if this doesn't work out)
+from pprint import pprint
+import requests
+import urllib.request
+#import
+from bs4 import BeautifulSoup     #try replaceing bs4 with BeautifulSoup
 from selenium import webdriver
 import pandas as pd 
-driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+#driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
 codes=[]
 questions=[]
-driver.get("https://www.codechef.com/problems/DEMTREE")
-content =driver.page_source
-soup=BeautifulSoup(content)
-for a in soup.findAll('a',href=True,attrs={'class':'ember251'}):
-	code=a.find('div', attrs={'class':'large-12 columns'})
-	codes.append(code)
-print(codes)
+#driver.get("https://www.codechef.com/problems/DEMTREE")
+#content =driver.page_source
+response = requests.get("https://www.codechef.com/problems/DEMTREE")
+soup = BeautifulSoup(response.text, "html.parser")
+#soup=BeautifulSoup(content)
+#print(soup)
+Text=soup.get_text()
+n=len(Text.split('\n'))
+print(n)
+A=Text.split('\n')
+pprint(A[290:n//2])
+# for a in soup.findAll('div'):
+# 	print(a,1)
+	#code=a.find('div', attrs={'class':'large-12 columns'})
+	#codes.append(a)
+#print(codes)
